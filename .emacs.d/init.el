c6da17cd2a7bc8e5aade58ab3ba573c8ed8277b6
tsujio <ntsujio@gmail.com>
2015-07-19 00:43:26 +0900

    Add setting for ruby indentation

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -272,6 +272,21 @@
 ;;;; indent setting for ruby-mode
 (setq ruby-deep-indent-paren-style nil)
 
+(defadvice ruby-indent-line (after unindent-closing-paren activate)
+  (let ((column (current-column))
+        indent offset)
+    (save-excursion
+      (back-to-indentation)
+      (let ((state (syntax-ppss)))
+        (setq offset (- column (current-column)))
+        (when (and (eq (char-after) ?\))
+                   (not (zerop (car state))))
+          (goto-char (cadr state))
+          (setq indent (current-indentation)))))
+    (when indent
+      (indent-line-to indent)
+      (when (> offset 0) (forward-char offset)))))
+
 ;;;; yasnippet
 (require 'yasnippet)
 (yas-global-mode 1)
@@ -399,3 +414,4 @@
 (require 'init-loader)
 (setq init-loader-show-log-after-init nil)
 (init-loader-load "~/.emacs.d/inits")
+
