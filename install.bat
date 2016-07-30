50bfa1664a12f2ebfb57b2a763a3b77431d183e7
tsujio <ntsujio@gmail.com>
2016-07-30 10:32:01 +0900

    Fix bug

--- install.bat
+++ install.bat
@@ -1,5 +1,7 @@
 @ECHO OFF
+SETLOCAL ENABLEDELAYEDEXPANSION
 FOR %%f IN (.zshrc,.gitconfig.windows,.emacs.d) DO (
-  MKLINK %HOME%\%%f %~dp0%%f
-  ECHO Created %HOME%\%%f
+  SET f=%%f
+  MKLINK %UserProfile%\!f:.windows=! %~dp0%%f
+  ECHO Created %UserProfile%\!f:.windows=!
 )
