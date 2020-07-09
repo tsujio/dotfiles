3dadb357165de72159627c6ecc7a769fa8400bf3
tsujio <ntsujio@gmail.com>
2020-07-09 22:12:01 +0900

    remove unused files

--- install.bat
+++ install.bat
@@ -1,4 +1,2 @@
 @ECHO OFF
-MKLINK %UserProfile%\.zshrc %~dp0.zshrc
-MKLINK %UserProfile%\.gitconfig %~dp0.gitconfig.windows
 MKLINK /D %UserProfile%\.emacs.d %~dp0.emacs.d
--- install.sh
+++ install.sh
@@ -2,6 +2,4 @@
 
 script_dir=$(cd $(dirname $0); pwd)
 
-[ -e ${HOME}/.zshrc ] || ln -s $script_dir/.zshrc ${HOME}/.zshrc
-[ -e ${HOME}/.gitconfig ] || ln -s $script_dir/.gitconfig.linux ${HOME}/.gitconfig
 [ -e ${HOME}/.emacs.d ] || ln -s $script_dir/.emacs.d ${HOME}/.emacs.d
