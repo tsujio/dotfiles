44017bdb654f4a6918fc3a47adb8c5e94cf4a3de
tsujio <ntsujio@gmail.com>
2020-07-10 21:29:33 +0900

    Add scala-mode

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -3,7 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (tabbar undo-tree fsharp-mode))))
+ '(package-selected-packages (quote (scala-mode tabbar undo-tree fsharp-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
